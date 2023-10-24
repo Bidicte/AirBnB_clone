9ee7d98402346b8443f9e965ecd3beb94de63afa
kouame konan abraham <abrahamkonany@gmail.com>
2023-10-24 11:30:05 +0000

    init project finish

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python3
-"""class BaseModeltributes/methods"""
-
+#!/usr/bin/env python3
+"""
+Class BaseModel that defines the attributes and methods
+for other classes
+"""
 from uuid import uuid4
 from datetime import datetime
-import models 
+import models
 
-class BaseModel:
-    """class of base"""
-    def __init__(self,*args, **kwargs):
-        """initialisation function
-        id: identification unique
-        created_at: date of create
-        update_at: date of update
-        """
+
+class BaseModel():
+    """ Class BaseModel that defines the attributes and methods
+    for other classes """
+    def __init__(self, *args, **kwargs):
+        """Constructor for a Class"""
+
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                if key == 'update_at' or key == 'created_at':
+                if key == 'updated_at' or key == 'created_at':
                     time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, time)
                 else:
@@ -25,22 +26,35 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at= self.created_at
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        """String representation of Object"""
+        sr = "[" + self.__class__.__name__ + "]"
+        sr += " (" + str(self.id) + ") "
+        sr += str(self.__dict__)
+        return (sr)
+
     def save(self):
-         """update datetime after modification"""
-         self.updated_at = datetime.now()
-         
+        """
+        Save into a file the Object converted in JSON String representation
+        """
+        new = datetime.now()
+        setattr(self, 'updated_at', new)
+        models.storage.save()
+
     def to_dict(self):
+        """
+        Returns a dictionary of an instance with a new key called '__class__'
+        and specific format for datetime keys
+        """
         new = {}
-        __dict = self.__dict__
-        for key in __dict:
-            if key == "create_at" or key == 'updated_at':
-                new[key] = __dict[key].isoformat()
+        _dict = self.__dict__
+        for key in _dict:
+            if key == 'created_at' or key == 'updated_at':
+                new[key] = _dict[key].isoformat()
             else:
-                new[key] = __dict[key]
+                new[key] = _dict[key]
         new['__class__'] = self.__class__.__name__
         return (new)
-    def __str__(self):
-        """print the instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-       
